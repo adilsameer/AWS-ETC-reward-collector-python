1fc3569cbf42de60cef9cddbe4ecf953870c80aa
Adil Sameer <63675452+adilsameer@users.noreply.github.com>
2023-12-21 21:15:02 +0530

    Update main.py
    Fixed captcha issue while sign in

--- main.py
+++ main.py
@@ -1,64 +1,37 @@
-import random
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
-from datetime import datetime as dt
+from selenium.common import TimeoutException
+from bot import Autobot
 import os
 
-
-# AWS ETC login url
-LOGIN_URL = "https://aws-emergingtalent.influitive.com/users/sign_in"
+# Demo https://replit.com/@AdilSameer/amazon-etc-auto-reward-collector-python
+# Github https://github.com/adilsameer/AWS-ETC-reward-collector-python
 
 # Enter your credentials below between "" quotes
-# # NOTE: You can also use environment variables for your credentials
 USER_EMAIL = "user@example.com"
-USER_PASSWORD = "password#1234"
+USER_PASSWORD = "pass@123"
+# # NOTE: You can also use environment variables for your credentials like below for Windows
+# USER_EMAIL = os.environ["AWS_EMAIL"]
+# USER_PASSWORD = os.environ["AWS_PASSWORD"]
+# And for replit environment variables (Secrets) use
+# USER_EMAIL = os.getenv("AWS_EMAIL")
+# USER_PASSWORD = os.getenv("AWS_PASSWORD")
 
-# This login funtion use your credentials to login on AWS ETC page
-def login():
-    email_input = driver.find_element(By.NAME, "user[email]")
-    password_input = driver.find_element(By.NAME, "user[password]")
-    email_input.send_keys(USER_EMAIL)
-    password_input.send_keys(USER_PASSWORD)
-    sign_in_btn = driver.find_element(By.ID, "sign-in-button")
-    sign_in_btn.send_keys(Keys.ENTER)
-
-# collect reward funtion is run after login with your credentials and collect reward
-def collect_reward():
+initial_time = time.time()
+keep_running = True
+while keep_running:
+    bot = Autobot()
     try:
-        reward = driver.find_element(By.CSS_SELECTOR, ".css-w09zei button")
-        reward.click()
-    except:
-        print("Reward already collected")
+        bot.user_login(USER_EMAIL, USER_PASSWORD)
+    except TimeoutException:
+        print("Invalid credentials")
+        bot.quit()
+        break
+    else:
+        bot.reward_collect()
+        bot.quit()
+        time_after_execution = time.time()
+        time_difference = time_after_execution - initial_time
+        remaining_time_to_wait = 24 * 60 * 60 - time_difference
+        time.sleep(remaining_time_to_wait)
 
-
-current_time = dt.now()
-
-# While loop is used to run program always
-while True:
-    # if condition check for current time is 1 in 24 hours (1 AM)
-    if current_time.hour == 1:
-        # random minutes choose random minute between 1 minute to 30 minutes
-        random_minutes = random.randint(60, 1800)
-        # program stops for random minutes
-        time.sleep(random_minutes)
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_experimental_option("detach", True)
-        # Driver open webpage
-        driver = webdriver.Chrome()
-        driver.get(LOGIN_URL)
-        login()
-        # After login to waits for page to load then collect reward
-        time.sleep(10)
-        collect_reward()
-        # After collecting reward it close chrome
-        driver.quit()
-        time_after_reward_collected = current_time.minute * 60
-        # Program sleeps for again 24 hours and rerun after 24  hours
-        sleep_24_hours = 86400 - time_after_reward_collected
-        time.sleep(sleep_24_hours)
-
-
-
-
+# Points collection from challenges(Tasks) is under testing
